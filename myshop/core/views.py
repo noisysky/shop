8dc4f58cfd1799baccdaeafe64b9e636eb84db87
noisysky <iana.vasylieva@gmail.com>
2018-11-13 07:30:37 +0200

    add context to index.html

--- myshop/core/views.py
+++ myshop/core/views.py
@@ -6,6 +6,6 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
-        context['listed'] = ["Featured products", "Recently viewed products", "New products", "Frequently purchased products"]
+        context['listed'] = ["Featured products", "New products", "Frequently purchased products", "Recently viewed products"]
 
         return context
