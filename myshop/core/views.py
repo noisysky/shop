f935a4e6366a9f933914d80b98c80a5e0f243fa3
noisysky <iana.vasylieva@gmail.com>
2018-11-13 07:16:39 +0200

    add greeting and current time to index.html

--- myshop/core/views.py
+++ myshop/core/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['listed'] = ["Featured products", "Recently viewed products", "New products", "Frequently purchased products"]
+
+        return context
