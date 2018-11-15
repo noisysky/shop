88d30590dee2bca2e2004e3cdd49919e9bd8e6b5
noisysky <iana.vasylieva@gmail.com>
2018-11-15 18:23:07 +0200

    add products to home page

--- myshop/core/views.py
+++ myshop/core/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import TemplateView
-
+from core.models import Products
 
 class Home(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
-        context['listed'] = ["Featured products", "New products", "Frequently purchased products", "Recently viewed products"]
-
+#        context['listed'] = ["Featured products", "New products", "Frequently purchased products", "Recently viewed products"]
+        products = Products.objects.all().select_related().order_by('price')[:10]  # JOIN for ForeignKey
+        context['products'] = products
         return context
