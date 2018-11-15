8d69b4e8b23282265f1b62b7f0c7b10038848ee6
noisysky <iana.vasylieva@gmail.com>
2018-11-15 18:02:10 +0200

    create models and populate the database

--- myshop/core/admin.py
+++ myshop/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import Products
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'count', 'type')#, 'get_total')
+    search_fields = ('name', 'trade_mark__name')
+
+#    def get_total(self, obj):
+#        return obj.price * obj.count
+#    get_total.short_description = 'Total'
+
+admin.site.register(Products, ProductAdmin)
--- myshop/core/models.py
+++ myshop/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    count = models.PositiveIntegerField()
+    type = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products')  # install pillow for use ImageField (pip install pillow)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+#    @property
+#    def total(self):
+#        return self.price * self.count
